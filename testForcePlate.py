463022839ff255dddfbe5d4ce31168f217d13869
Robert Ellenberg <rwe24@drexel.edu>
2012-08-02 12:34:25 -0400

    Fixed force sensor demo

--- testForcePlate.py
+++ testForcePlate.py
@@ -10,7 +10,16 @@ if __name__ == "__main__":
         env.SetViewer('qtcoin')
         env.Load('forcePlate.env.xml') 
         
+        physics = RaveCreatePhysicsEngine(env,'ode')
+        physics.SetGravity([0,0,-9.8])
+        env.SetPhysicsEngine(physics)
+
+        #The first robot defined in the XML file is Jaemi Hubo
         robot = env.GetRobots()[0]
+        #The second "robot" is the force plate, which is grossly oversized to
+        #prevent weird collision issues, like the plates completely intersecting
+        #each other.
+        plate = env.GetRobots()[1]
         forceSensor = robot.GetAttachedSensors()[0].GetSensor()
     
         force = 0
@@ -21,16 +30,22 @@ if __name__ == "__main__":
         sumTorqueBody = 0
         with env:
             robot = env.GetRobots()[0]
-            robot.SetController(RaveCreateController(env,'odevelocity'),range(robot.GetDOF()),0)
+            #robot.SetController(RaveCreateController(env,'odevelocity'),range(robot.GetDOF()),0)
+            robot.SetController(RaveCreateController(env,'servocontroller'))
+            collisionChecker = RaveCreateCollisionChecker(env,'bullet')
+            env.SetCollisionChecker(collisionChecker)
+
             env.StopSimulation()
-            env.StartSimulation(timestep=0.0001 )
+            env.StartSimulation(timestep=0.001 )
 
+            robot.GetController().SendCommand('setpos1 0 20 ')
 
-        velocities = numpy.zeros(robot.GetDOF())
-        robot.GetController().SendCommand('setvelocity '+' '.join(str(f) for f in velocities))
-        for i in xrange(10000):
+        #velocities = numpy.zeros(robot.GetDOF())
+        #robot.GetController().SendCommand('setvelocity '+' '.join(str(f) for f in velocities))
+        for i in xrange(100):
+            time.sleep(.1)
             data = forceSensor.GetSensorData()
-            [force,torque] = env.GetPhysicsEngine().GetLinkForceTorque( robot.GetLink('Plate') )
+            [force,torque] = env.GetPhysicsEngine().GetLinkForceTorque( plate.GetLink('Plate') )
             print data.force, data.torque, force, torque
             
         raw_input('')
--- testJointMovement.py
+++ testJointMovement.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         
         env = Environment()
         env.SetViewer('qtcoin')
-        env.Load('jaemiHubo.fullDOF.robot.xml') 
+        env.Load('jaemiHubo.robot.xml') 
         time.sleep(1)
         
         physics = RaveCreatePhysicsEngine(env,'ode')
@@ -24,11 +24,20 @@ if __name__ == "__main__":
             env.StopSimulation()
             env.StartSimulation(timestep=0.001 )
 
+        
+        w=0.5*2.0*numpy.pi
+        T=1.0/w
+        steps=25
+        A=.1
+        runtime=10
 
-        for i in range(360):
-            velocities = numpy.zeros(robot.GetDOF())+numpy.sin(i*numpy.pi/180)*.1
+        for i in range(runtime*steps):
+            t=float(i)/steps
+            #print t
+            velocities = numpy.zeros(robot.GetDOF())+numpy.sin(w*t)*A
+            #print velocities
             robot.GetController().SendCommand('setvelocity '+' '.join(str(f) for f in velocities))
-            time.sleep(.02)
+            time.sleep(1.0/float(steps))
 
         velocities = numpy.zeros(robot.GetDOF())
         robot.GetController().SendCommand('setvelocity '+' '.join(str(f) for f in velocities))
