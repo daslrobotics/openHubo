ed90d9af1419e33b3f901ef01188f0f02418579a
Robert Ellenberg <rwe24@drexel.edu>
2012-08-05 04:35:29 -0400

    added CoMPS plugins and updated build system to install local plugins by default

--- setup-plugins.sh
+++ setup-plugins.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
+BASE_DIR=`git rev-parse --show-toplevel`
+
 git submodule update --init
-cd openmr/plugin
+cd $BASE_DIR/openmr/
+[ -d build ] || mkdir build
+cd build
+cmake ../src/
+make
+sudo make install
+
+cd $BASE_DIR/forceSensor
 cmake .
 make
 sudo make install
-cd ../../forceSensor
-cmake .
-make
-sudo make install
+
+for pkg in generalik cbirrt2 manipulation2
+do
+   cd $BASE_DIR/comps-plugins/$pkg
+   [ -d build ] || mkdir build
+   cd build
+   cmake ../
+   make
+   sudo make install
+done
+
+cd $BASE_DIR
+
+SOURCED_SEARCH=`grep OPENRAVE_PLUGINS ~/.bashrc`
+
+if [[ ${#SOURCED_SEARCH} == 0 ]]
+then
+    echo 'OPENRAVE_PLUGINS=$OPENRAVE_PLUGINS:'$BASE_DIR'/plugins' >> ~/.bashrc
+fi
+
+export OPENRAVE_PLUGINS=$OPENRAVE_PLUGINS:$BASE_DIR/plugins
