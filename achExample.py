f1bce9ede49f5c7487cc9c586a8918b5b8b8db18
Robert Ellenberg <rwe24@drexel.edu>
2012-08-29 18:49:45 -0400

    added a fake read function for ACH packets, physics is acting odd with .001 timestep

--- achExample.py
+++ achExample.py
@@ -5,7 +5,6 @@ from numpy import *
 import time
 import sys
 
-
 def run():
 
     #-- Read the name of the xml file passed as an argument
@@ -30,15 +29,16 @@ def run():
         #define ODE physics engine and set gravity
 
         env.StopSimulation()
-        env.StartSimulation(timestep=0.001)
-
-    mypos1={'TY':45,'LHR':20,'LHY':20,'LHP':0}
-    setACHReference(robot,mypos1)
+        env.StartSimulation(timestep=0.0005)
     time.sleep(2)
-    mypos2={'TY':0,'LHR':20,'LHY':20,'LHP':0}
-    setACHReference(robot,mypos2)
 
-    raw_input('Press any key to quit...')
+    setACHReference(robot,{'LSR':50,'RSR':-50,'REP':-20,'LEP':-20})
+
+    while True:
+        setACHReference(robot,readACHPacket())
+        time.sleep(.1)
+    
+
 
 def setACHReference(robot,refPos):
     for x in refPos.keys():
@@ -48,6 +48,16 @@ def setACHReference(robot,refPos):
         print cmd
         robot.GetController().SendCommand(cmd)
 
+def readACHPacket():
+    #Eventually use this function to access the ACH channel and get the most
+    #recent set point. For now, make up random values to send
+    setPoint={'LSP':0.0,'LSR':80.0,'LSY':0.0,'LEP':-50,'LWY':0.0,'LWP':0.0}
+    for x in setPoint.keys():
+        setPoint[x]+=numpy.random.rand()*20.0-10.0
+        #TODO: figure out why robot explodes when setpoint is too far past limit
+        #(probably have to query limits to fix this).
+
+    return setPoint
 
 if __name__=='__main__':
     run()
