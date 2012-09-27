217874c0f812294907aa3575852522ed781aad03
Robert Ellenberg <rwe24@drexel.edu>
2012-09-27 17:04:08 -0400

    Updated testServo to be a testbed for various servo functions

--- interactive_sandbox.py
+++ interactive_sandbox.py
@@ -23,20 +23,8 @@ import datetime
 import sys
 import tab
 
-""" Demonstrate the range of motion of a joint by attempting to reach +/- 180
-deg"""
-def testMotionRange(robot,jointName,steps=50):
-    joint=robot.GetJoint(jointName)
-    dq=360.0
-    q0=-180.0
-
-    for k in [x*dq/steps+q0 for x in range(steps+1)]:
-        print k
-        robot.GetController().SendCommand('setpos1 {} {}'.format(joint.GetDOFIndex(),k))
-        time.sleep(.1)
-
-
 if __name__=='__main__':
+
     #-- Read the name of the xml file passed as an argument
     #-- or use the default name
     try:
@@ -56,12 +44,10 @@ if __name__=='__main__':
         robot.SetController(RaveCreateController(env,'servocontroller'))
         collisionChecker = RaveCreateCollisionChecker(env,'ode')
         env.SetCollisionChecker(collisionChecker)
+        LSR=robot.GetJoint('LSR').GetDOFIndex()
+        LEP=robot.GetJoint('LEP').GetDOFIndex()
 
         env.StopSimulation()
         #Use .0005 timestep for non-realtime simulation with ODE to reduce jitter.
         env.StartSimulation(timestep=0.0005)
 
-    time.sleep(2)
-
-    #Begin experimental sandbox here:
-    testMotionRange(robot,'LSR')
