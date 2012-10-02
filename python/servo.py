80567105061503d696a63b00d9850b3db3962a35
Robert Ellenberg <rwe24@drexel.edu>
2012-10-02 14:00:18 -0400

    Servo functions and foot base

--- python/servo.py
+++ python/servo.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-# 08-16-12 OpenRave Tutorial Test Code 
-# Hubo
 
-from openravepy import *
-import time
-import scipy
+""" OpenHubo servo functions """
+
 import tab
+from openravepy import *
 from numpy import *
 from numpy.linalg import *
 import sys
-import datetime
+import time
 
-def sendServoCommand(robot,trunk=zeros(4),la=zeros(7),ra=zeros(7),ll=zeros(6),rl=zeros(6),lf=zeros(15),rf=zeros(15)):
+#TODO: Work with the concept of activeDOF?
+
+def sendServoCommandByLimb(robot,trunk=zeros(4),la=zeros(7),ra=zeros(7),ll=zeros(6),rl=zeros(6),lf=zeros(15),rf=zeros(15)):
     #Ugly method of joining up manipulator arrays
     #TODO: Error checking in array length?
     joints=trunk.copy()
@@ -23,6 +23,30 @@ def sendServoCommand(robot,trunk=zeros(4),la=zeros(7),ra=zeros(7),ll=zeros(6),rl
     #for f in range(0,60):
     #    print "{}: {}".format(robot.GetJointFromDOFIndex(f).GetName(),joints[f])
 
+""" Send an array of servo positions directly to the robot """
+def sendServoCommand(robot,raw):
+    #build command string to pass to the servo controller.
+    positions=array(zeros(len(robot.GetDOF())))
+    for k in range(len(raw)):
+        positions[k]=raw[k]
+
+    strtmp = 'setpos '+' '.join(str(f) for f in positions)
+    robot.GetController().SendCommand(strtmp)
+
+""" Update only joints that are specified in the dictionary (not implemented
+yet) """
+def sendSparseServoReference(robot,posDict):
+    positions=zeros(len(robot.GetDOF()))
+    print "Not yet implented!"
+
+def sendSingleJointTrajectory(robot,trajectory,jointID,timestep=.1):
+    """ Send a trajectory that will be played back for a single joint """
+    #TODO: time by sim timesteps (i.e. manually step simulation)
+    for k in trajectory:
+        strtmp = 'setpos1 {} {}'.format(jointID,k)
+        robot.GetController().SendCommand(strtmp)
+        time.sleep(timestep)
+        
 
 if __name__=='__main__':
     try:
@@ -45,7 +69,7 @@ if __name__=='__main__':
         env.StopSimulation()
         env.StartSimulation(timestep=0.001)
 
-    time.sleep(3)
+    time.sleep(1)
     # Avoid setting servo Kp higher than about 1/20 of the update frequency
     #robot.GetController().SendCommand('setgains 50 .5 2  .1 .1')
     #robot.GetController().SendCommand('setpos 0 0 30 -30 0 50')
@@ -57,11 +81,19 @@ if __name__=='__main__':
     qRA = array([0,-0 + offset_RSR,0,0,0,0,0])
     qLA[3] = -30
     qRA[3] = -30
-    qLL = array([0,0,-30,60,-30,0])/2
-    qRL = array([0,0,-30,60,-30,0])/2
+    qLL = array([0,0,-30,60,-30,0])/20
+    qRL = array([0,0,-30,60,-30,0])/20
 
-    robot.GetController().SendCommand('setgains 50 1 5 .1 .1')
+    robot.GetController().SendCommand('setgains 50 0 3 .1 .1')
 
-    sendServoCommand(robot,qW,qLA,qRA,qLL,qRL)
+    sendServoCommandByLimb(robot,qW,qLA,qRA,qLL,qRL)
+
+    time.sleep(1)
+
+    t=array([k/20.0 for k in range(100)])
+    A=20
+    traj=cos(2.0*pi*t)*A-A
+
+    sendSingleJointTrajectory(robot,traj,robot.GetJoint('LEP').GetDOFIndex(),.05)
     #Run this in interactive mode to preserve the state
 
