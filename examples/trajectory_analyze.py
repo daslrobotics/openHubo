a961c07581425c0b1be5581912bd52d670787837
Robert Ellenberg <rwe24@drexel.edu>
2013-05-06 14:45:46 -0400

    Removed old files

--- examples/trajectory_analyze.py
+++ examples/trajectory_analyze.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #// This program is free software: you can redistribute it and/or modify
 #// it under the terms of the GNU Lesser General Public License as published by
 #// the Free Software Foundation, either version 3 of the License, or
@@ -16,46 +15,23 @@ from __future__ import with_statement # for python 2.5
 __author__ = 'Robert Ellenberg'
 __license__ = 'GPLv3 license'
 
-from openravepy import *
-from numpy import *
-from trajectory import *
-import time
-import datetime
-import sys
+from openhubo import trajectory
 import openhubo
-
+import openravepy as rave
+import time
 
 if __name__=='__main__':
-    #TODO: openhubo refactor
-    from cbirrt import *
-
-    # Read the name of the xml file passed as an argument
-    # or use the default name
-    try:
-        file_env = sys.argv[1]
-    except IndexError:
-        file_env = 'rlhuboplus.robot.xml'
-
-    env = Environment()
-    #(env,options)=openhubo.setup('qtcoin')
-    env.SetDebugLevel(1)
-
-    # Set the robot controller and start the simulation
-    with env:
-        env.Load(file_env)
-        robot = env.GetRobots()[0]
-        collisionChecker = RaveCreateCollisionChecker(env,'ode')
-        env.SetCollisionChecker(collisionChecker)
+    (env,options)=openhubo.setup('qtcoin')
+    [robot,ind,__,__,__]=openhubo.load_scene(env,options)
 
     #Read in trajectory from matlab source
-    traj=read_text_traj('trajectories/ingress_test.traj.txt',robot,0.01,1.0)
+    traj=trajectory.read_text_traj('trajectories/ingress_test.traj.txt',robot,0.01,1.0)
     config=traj.GetConfigurationSpecification()
-    ind=openhubo.makeNameToIndexConverter(robot)
 
-    Getvals=makeJointValueExtractor(robot,traj,config)
-    Gettrans=makeTransformExtractor(robot,traj,config)
-    
-    probs_cbirrt = RaveCreateProblem(env,'CBiRRT')
+    Getvals=trajectory.makeJointValueExtractor(robot,traj,config)
+    Gettrans=trajectory.makeTransformExtractor(robot,traj,config)
+
+    probs_cbirrt = rave.RaveCreateProblem(env,'CBiRRT')
     env.LoadProblem(probs_cbirrt,'rlhuboplus')
     i=0
     t0=time.time()
