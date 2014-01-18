33d4c1c75f99ebde73b1444e690c6f676477c948
Robert W. Ellenberg <rwe24g@gmail.com>
2014-01-18 17:25:29 -0500

    Fixed basis for friction vectors

--- openhubo/cws.py
+++ openhubo/cws.py
@@ -1,45 +1,71 @@
 from openravepy import CollisionReport
-from numpy import zeros, cross, array, pi
+from numpy import zeros, cross, array, pi, cos, sin, mat
 import openhubo as oh
 from openhubo.comps import Transform
 from scipy.spatial import ConvexHull
+from scipy.spatial.qhull import QhullError
 import numpy as np
+from nullspace import nullspace
 
 #TODO declare a vector of peak forces for each contact body?
-def perform_cws(robot,links):
-    env=robot.GetEnv()
-    report=CollisionReport()
 
-    m = oh.find_mass(robot)
-    g = 9.81
+class ContactCheck:
+    def __init__(self,robot, mu=0.2):
+        self.robot = robot
+        self.env = robot.GetEnv()
+        self.links = {}
+        self.mu = mu
 
-    CWS=[array([0,0,0,0,0])]
-    for l in links:
-        link = robot.GetLink(l)
-        env.CheckCollision(link,report)
-        for c in report.contacts:
-            for theta in [0,pi/2,pi,3*pi/2]:
-                w = zeros(6)
-                # arbitrary scale factor
-                w[0:3] = (c.norm+array([np.cos(theta),np.sin(theta),0])*.1) * 2 *m*g
-                w[3:] = cross(c.pos, w[0:3])
-                CWS.append(w[0:5])
+    def insert_link(self,linkname):
+        l = robot.GetLink(linkname)
+        if l is not None:
+            self.links[linkname]=l
 
-    w_g = zeros(6)
+    def enable(self):
+        for n,l in self.links.items():
+            l.Enable(True)
 
-    r = oh.find_com(robot)
-    m = oh.find_mass(robot)
+    def disable(self):
+        for n,l in self.links.items():
+            l.Enable(False)
 
-    w_g[0:3] = m*array([0,0,g])
-    w_g[3:6] = cross(r,w_g[0:3])
+    def perform_cws(self):
+        report=CollisionReport()
 
-    #TODO evaluate convex hull of CWS here
-    hull = ConvexHull(array(CWS))
+        m = oh.find_mass(self.robot)
+        g = 9.81
 
-    inside = check_interior(hull,w_g[0:5])
+        self.CWS=[array([0,0,0,0,0])]
+        for n,l in self.links.items():
 
-    #TODO reduce CWS with simplified contacts
-    return (inside, hull,CWS, report)
+            env.CheckCollision(l,report)
+            for c in report.contacts:
+                basis = nullspace(mat(c.norm))
+                for theta in [0,pi/2,pi,3*pi/2]:
+                    w = zeros(6)
+                    # arbitrary scale factor
+                    vec = cos(theta) * array(basis[:,0])+sin(theta)*array(basis[:,1])
+                    w[0:3] = np.squeeze(array(c.norm) + np.squeeze(vec) * self.mu) * 2 * m*g
+                    w[3:] = cross(c.pos, w[0:3])
+                    self.CWS.append(w[0:5])
+
+        w_g = zeros(6)
+
+        r = oh.find_com(self.robot)
+        m = oh.find_mass(self.robot)
+
+        w_g[0:3] = m*array([0,0,g])
+        w_g[3:6] = cross(r,w_g[0:3])
+
+        #TODO evaluate convex hull of CWS here
+        try:
+            self.hull = ConvexHull(array(self.CWS))
+        except QhullError:
+            print "can't build CWS, assuming unstable!"
+            return False
+        inside = check_interior(self.hull,w_g[0:5])
+
+        return inside
 
 def check_interior(hull, point, thorough=False):
     """Slowest possible way to check for interior, by brute force over all planes."""
@@ -63,8 +89,14 @@ def test_cws(robot):
     robot.SetTransform(T)
     print "Move robot to colliding position"
     oh.pause()
-    stable, hull, CWS, report = perform_cws(robot,['leftFoot','rightFoot','leftPalm','rightPalm'])
-    return stable
+    check=ContactCheck(robot)
+    check.insert_link('leftFoot')
+    check.insert_link('leftPalm')
+    check.insert_link('rightFoot')
+    check.insert_link('rightPalm')
+    print check.perform_cws()
+    return check
+
 
 class ContactSphere:
     suffix=0
@@ -106,7 +138,7 @@ class ContactSphereSet:
         contacts = [s.check() for s in self.spheres]
         return contacts
 
-class CWSCheck:
+class SphereCheck:
     def __init__(self,robot):
         self.robot = robot
         self.contactsets={}
@@ -120,7 +152,7 @@ class CWSCheck:
             s.update()
             self.activecontacts.setdefault(l,s.check())
 
-f_standoff = 0.003
+f_standoff = 0.004
 def create_right_foot(robot):
     rightFootSpheres=ContactSphereSet()
     rightFootSpheres.append(ContactSphere(robot,Transform(trans=[0.063,0.058,-f_standoff]),'rightFoot'))
@@ -137,7 +169,7 @@ def create_left_foot(robot):
     leftFootSpheres.append(ContactSphere(robot,Transform(trans=[-0.133,0.065,-f_standoff]),'leftFoot'))
     return leftFootSpheres
 
-h_standoff = 0.0006
+h_standoff = 0.001
 
 def create_right_palm(robot):
     rightPalmSpheres=ContactSphereSet()
@@ -158,21 +190,31 @@ def create_left_palm(robot):
 if __name__ == '__main__':
 
     (env,options)=oh.setup('qtcoin',True)
-    env.SetDebugLevel(3)
+    env.SetDebugLevel(4)
     #TODO fix hard name here
     options.robotfile='../robots/drchubo/drchubo_v3/robots/drchubo_v3.robot.xml'
+    options.scenefile='../scenes/ladderclimb.env.xml'
     [robot,ctrl,ind,ref,recorder]=oh.load_scene(env,options)
 
 
-    check = CWSCheck(robot)
-    check.insert_contacts('rightFoot', create_right_foot(robot))
-    check.insert_contacts('leftFoot', create_left_foot(robot))
-    check.insert_contacts('rightPalm', create_right_palm(robot))
-    check.insert_contacts('leftPalm', create_left_palm(robot))
+    #check = CWSCheck(robot)
+    #check.insert_contacts('rightFoot', create_right_foot(robot))
+    #check.insert_contacts('leftFoot', create_left_foot(robot))
+    #check.insert_contacts('rightPalm', create_right_palm(robot))
+    #check.insert_contacts('leftPalm', create_left_palm(robot))
+    oh.pause()
+    env.StartSimulation(oh.TIMESTEP)
     pose=oh.Pose(robot)
     pose['LF1']=-pi/3
     pose['RF1']=-pi/3
     pose['RF2']=-pi/3
     pose.send()
-    check.build_active_sets()
+    #check.build_active_sets()
 
+    check = test_cws(robot)
+
+    for k in xrange(100):
+        oh.pause(0.5)
+        env.StopSimulation()
+        print check.perform_cws()
+        env.StartSimulation(oh.TIMESTEP)
